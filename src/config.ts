123895eadb70b8bb22ba0692463b2c2ab417ec62
ryan <ryanf39@gmail.com>
2024-05-22 10:38:19 -0400

    robots.txt

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'RyanF.dev';
-export const SITE_DESCRIPTION = 'RyanF.dev is a portfolio and blog website for content about coding and cloud engineering.';
+export const SITE_DESCRIPTION = 'Building, writing and sharing resouces related to automation in the cloud. Mostly AWS, but always adding new Infrastructure-as-Code(IaC) and general programming/development content.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
